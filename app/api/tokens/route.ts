2477fc0b6cba5a306df96fc5d94b24c9d5c95148
dorkydhruv <thedhruvsharma@gmail.com>
2024-08-16 03:24:36 +0500

    token fixes

--- app/api/tokens/route.ts
+++ app/api/tokens/route.ts
@@ -8,21 +8,26 @@ import {
 } from "@solana/spl-token";
 
 export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const address = searchParams.get("address") as unknown as string;
-  const supportedToken = await getSupportedTokens();
-  const balances = await Promise.all(
-    supportedToken.map(async (token) => getAccountBalance(token, address))
-  );
-  const tokens = supportedToken.map((token, index) => ({
-    ...token,
-    balance: balances[index],
-    usdBalance: balances[index] * Number(token.price),
-  }));
-  return NextResponse.json({
-    tokens,
-    totalBalance: tokens.reduce((acc, val) => acc + val.usdBalance, 0),
-  });
+  try {
+    const { searchParams } = new URL(req.url);
+    const address = searchParams.get("address") as unknown as string;
+    const supportedToken = await getSupportedTokens();
+    const balances = await Promise.all(
+      supportedToken.map(async (token) => getAccountBalance(token, address))
+    );
+    const tokens = supportedToken.map((token, index) => ({
+      ...token,
+      balance: balances[index],
+      usdBalance: balances[index] * Number(token.price),
+    }));
+    return NextResponse.json({
+      tokens,
+      totalBalance: tokens.reduce((acc, val) => acc + val.usdBalance, 0),
+    });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ error: e });
+  }
 }
 
 async function getAccountBalance(
@@ -34,15 +39,16 @@ async function getAccountBalance(
   },
   address: string
 ) {
-  if (token.native) {
-    let balance = await connection.getBalance(new PublicKey(address));
-    return balance / LAMPORTS_PER_SOL;
-  }
-  const ata = await getAssociatedTokenAddress(
-    new PublicKey(token.mint),
-    new PublicKey(address)
-  );
   try {
+    if (token.native) {
+      let balance = await connection.getBalance(new PublicKey(address));
+      return balance / LAMPORTS_PER_SOL;
+    }
+    const ata = await getAssociatedTokenAddress(
+      new PublicKey(token.mint),
+      new PublicKey(address)
+    );
+
     const account = await getAccount(connection, ata);
     return Number(account.amount) / 10 ** token.decimals;
   } catch (e) {
